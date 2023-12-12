08c8b1ac918385aa98eeb3befbf650ad9539224d
sakumatakumi <goose.326.hose.bibi@gmail.com>
2023-12-13 02:33:50 +0900

    フォームファザードを使うための設定

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // フォームファザードhtmlコード省略（laravelで使えるように）
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +229,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //フォームファザードオン
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];

d985b2111d92bb51cfac56c89cd7e88699963d6f
sakumatakumi <goose.326.hose.bibi@gmail.com>
2023-12-12 15:29:55 +0900

    マイグレーションしたファイル

--- database/migrations/2019_12_24_090209_create_posts_table.php
+++ database/migrations/2019_12_24_090209_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->integer('user_id');
-            $table->string('post',400);
+            $table->string('post', 400);
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
         });

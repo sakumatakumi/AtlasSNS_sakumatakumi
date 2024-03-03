1032a0c9b4852ae9e29b2f5706433a9ecc0ace17
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-03-03 14:53:52 +0900

    投稿場所のアイコン表示修正。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <div class="posts">
   {{ Form::open(['route' => 'posts']) }}
-  <img src="{{ asset('storage/profileImages/' . Auth::user()->images) }}" alt="User Icon" class="posts__icon">
+  @if(Auth::user()->images == "icon1.png")
+  <img src="{{ asset('images/icon1.png') }}" alt="User Icon" class="posted__icon">
+  @else
+  <img src="{{asset('storage/profileImages/' . Auth::user()->images)}}" alt="User Icon" class="posted__icon">
+  @endif
   {{ Form::textarea('post', null,['required', 'class' => 'posts__text', 'placeholder' => '投稿内容を入力してください。'])}}
   <input type="submit" class="posts__btn" name="posts__btn">
   {!! Form::close() !!}
@@ -13,14 +17,14 @@
 <!-- 繰り返し（foreach））php読み返し -->
 <div class="posted">
   @if($post->user->images == "icon1.png")
-  <img src="images/icon1.png" alt="User Icon" class="posted__icon">
+  <img src="{{asset('images/icon1.png')}}" alt="User Icon" class="posted__icon">
   @else
   <img src="{{asset('storage/profileImages/' . $post->user->images)}}" alt="User Icon" class="posted__icon">
   @endif
   <p class="posted__name">{{ $post->user->username }}</p>
   <!--改行有りのタグ-->
   <p class="posted__post">{{ $post->post }}</p>
-  <p class="posted__time">{{ $post->updated_at }}</p>
+  <p class="posted__time">{{ $post->created_at }}</p>
   <!-- ユーザーだけ表示 if文で区分してあげる。自分とその他。 -->
   @if($post->user_id == Auth::id())
   <div class="posted__edit">

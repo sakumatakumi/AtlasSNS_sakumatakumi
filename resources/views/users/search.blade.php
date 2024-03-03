0a74e9630676b3cf39a21db8f58d6be075d4925f
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-03-03 14:53:01 +0900

    follow機能修正

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -18,17 +18,14 @@
 
 @foreach ($users as $user)
 <div class="user__list">
-  <img src="images/icon1.png" alt="User Icon" class="list--icon">
+  @if($user->images == "icon1.png")
+  <img src="{{ asset('images/icon1.png') }}" alt="User Icon" class="posted__icon">
+  @else
+  <img src="{{asset('storage/profileImages/' . $user->images)}}" alt="User Icon" class="posted__icon">
+  @endif
   <p class="posted__name">{{ $user->username }}</p>
 
-  <form action="{{ route('follow', $user->id) }}" method="POST">
-    @csrf
-    <input type="hidden" name="user_id_to_follow" value="{{ $user->id }}">
-    <button type="submit" class="follow--btn">
-      フォローする
-    </button>
-  </form>
-
+  @if(auth()->user()->isFollowing($user->id))
   <form action="{{ route('unfollow', $user->id) }}" method="POST">
     @csrf
     <input type="hidden" name="user_id_to_unfollow" value="{{ $user->id }}">
@@ -36,6 +33,15 @@
       フォロー解除
     </button>
   </form>
+  @else
+  <form action="{{ route('follow', $user->id) }}" method="POST">
+    @csrf
+    <input type="hidden" name="user_id_to_follow" value="{{ $user->id }}">
+    <button type="submit" class="follow--btn">
+      フォローする
+    </button>
+  </form>
+  @endif
 </div>
 @endforeach
 

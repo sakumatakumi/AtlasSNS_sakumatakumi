58351c50afbddec0dfb2436b00966ddc283b0b7c
sakumatakumi <goose.326.hose.bibi@gmail.com>
2024-04-25 23:00:57 +0900

    CSS調整

--- resources/views/users/otherProfile.blade.php
+++ resources/views/users/otherProfile.blade.php
@@ -3,49 +3,71 @@
 @section('content')
 
 <div class="otherProfile">
-  @if($user->images == "icon1.png")
-  <img src="{{ asset('images/icon1.png') }}" alt="User Icon" class="posted__icon">
-  @else
-  <img src="{{asset('storage/profileImages/' . $user->images)}}" alt="User Icon" class="posted__icon">
-  @endif
-
-  <p class="">ユーザー名</p>
-  <p class="otherProfile__name">{{ $user->username }}</p>
-  <p class="">自己紹介</p>
-  <p class="otherProfile__bio">{{ $user->bio }}</p>
-
-  @if(auth()->user()->isFollowing($user->id))
-  <form action="{{ route('unfollow', $user->id) }}" method="POST">
-    @csrf
-    <input type="hidden" name="user_id_to_unfollow" value="{{ $user->id }}">
-    <button type="submit" class="unfollow--btn">
-      フォロー解除
-    </button>
-  </form>
-  @else
-  <form action="{{ route('follow', $user->id) }}" method="POST">
-    @csrf
-    <input type="hidden" name="user_id_to_follow" value="{{ $user->id }}">
-    <button type="submit" class="follow--btn">
-      フォローする
-    </button>
-  </form>
-  @endif
-
-  @foreach ($post as $post)
-  @if($post->user->images == "icon1.png")
-  <img src="{{asset('images/icon1.png')}}" alt="User Icon" class="posted__icon" class="icon">
-  @else
-  <img src="{{asset('storage/profileImages/' . $post->user->images)}}" alt="User Icon" class="posted__icon" class="icon">
-  @endif
-  <p class="posted__name">{{ $post->user->username }}</p>
-  <!--改行有りのタグ-->
-  <p class="posted__post">{{ $post->post }}</p>
-  <p class="posted__time">{{ $post->created_at }}</p>
-  @endforeach
-
-
-
+  <div class="otherProfile__img">
+    @if($user->images == "icon1.png")
+    <img src="{{ asset('images/icon1.png') }}" alt="User Icon" class="posted__icon">
+    @else
+    <img src="{{asset('storage/profileImages/' . $user->images)}}" alt="User Icon" class="posted__icon">
+    @endif
+  </div>
+  <ul class="otherProfile--item">
+    <li>
+      <p>ユーザー名</p>
+      <p>{{ $user->username }}</p>
+    </li>
+    <li>
+      <p>自己紹介</p>
+      <p>{{ $user->bio }}</p>
+    </li>
+  </ul>
+  <div class="otherProfile__btn">
+    @if(auth()->user()->isFollowing($user->id))
+    <form action="{{ route('unfollow', $user->id) }}" method="POST">
+      @csrf
+      <input type="hidden" name="user_id_to_unfollow" value="{{ $user->id }}">
+      <button type="submit">
+        フォロー解除
+      </button>
+    </form>
+    @else
+    <form action="{{ route('follow', $user->id) }}" method="POST">
+      @csrf
+      <input type="hidden" name="user_id_to_follow" value="{{ $user->id }}">
+      <button type="submit">
+        フォローする
+      </button>
+    </form>
+    @endif
+  </div>
 </div>
+
+@foreach ($post as $post)
+<div class="posted">
+  <div class="posted__icon">
+    @if($post->user->images == "icon1.png")
+    <img src="{{asset('images/icon1.png')}}" alt="User Icon" class="posted__icon" class="icon">
+    @else
+    <img src="{{asset('storage/profileImages/' . $post->user->images)}}" alt="User Icon" class="posted__icon" class="icon">
+    @endif
+  </div>
+  <div class="posted__text">
+    <div class="posted__name">
+      <p class="posted__name">{{ $post->user->username }}</p>
+    </div>
+    <div class="posted__post">
+      <!--改行有りのタグ-->
+      <p class="posted__post">{{ $post->post }}</p>
+    </div>
+  </div>
+  <div class="posted__item">
+    <div class="posted__time">
+      <p class="posted__time">{{ $post->created_at }}</p>
+    </div>
+  </div>
+</div>
+@endforeach
+
+
+
 
 @endsection
